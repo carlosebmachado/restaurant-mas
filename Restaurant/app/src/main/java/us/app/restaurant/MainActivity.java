4bfb5533d43f9ce9b5b875bdc2f667f03a21ce46
Carlos Eduardo de Borba Machado <carlosebmachado@outlook.com>
2021-09-25 14:44:27 -0300

    popup example

--- Restaurant/app/src/main/java/us/app/restaurant/MainActivity.java
+++ Restaurant/app/src/main/java/us/app/restaurant/MainActivity.java
@@ -1,14 +1,17 @@
 package us.app.restaurant;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ScrollView;
 import android.widget.Toast;
 
 import com.google.android.material.appbar.AppBarLayout;
+import com.google.android.material.snackbar.Snackbar;
 
 import jade.core.Agent;
 
@@ -29,6 +32,9 @@ public class MainActivity extends AppCompatActivity {
     public void search(View v){
         Toast.makeText(this, "Procurando...", Toast.LENGTH_SHORT).show();
 
+        // exemplo de popup
+        popup();
+
         for (int i = 0; i < 4; ++i){
             Fragment fragment = RestaurantCard.newInstance(RestaurantData.data[i][RestaurantData.PHOTO],
                                                            RestaurantData.data[i][RestaurantData.NAME],
@@ -42,4 +48,30 @@ public class MainActivity extends AppCompatActivity {
 
         scrollView.setVisibility(View.VISIBLE);
     }
+
+    private void popup(){
+        // provavelmente será criada uma classe
+        // referencia: https://developer.android.com/guide/topics/ui/dialogs?hl=pt-br
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        // Add the buttons
+        builder.setPositiveButton("Sim", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User clicked OK button
+            }
+        });
+        builder.setNegativeButton("Não", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                // User cancelled the dialog
+            }
+        });
+        // Set other dialog properties
+        builder.setMessage("Hm... Achamos apenas 1 restaurante desse tipo perto de você.\nVocê se importaria de andar por pelo menos 10km?").setTitle("Assistente");
+
+        // Create the AlertDialog
+        AlertDialog dialog = builder.create();
+
+        dialog.show();
+    }
+    
 }
